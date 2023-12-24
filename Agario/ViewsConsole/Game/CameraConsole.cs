3b3ab1c50084b50e939792057fbdece538b1e8ff
Pavel <gpv2002@mail.ru>
2023-12-25 00:08:03 +0300

    Масштабирование камеры

--- Agario/ViewsConsole/Game/CameraConsole.cs
+++ Agario/ViewsConsole/Game/CameraConsole.cs
@@ -30,10 +30,37 @@ namespace ViewsConsole.Game
     }
 
     /// <summary>
+    /// Масштабирование камеры по размерам игрока
+    /// </summary>
+    private void AdjustToPlayerSize()
+    {
+      /* Минимальный и максимальный масштабный коэффициент размеров игрока относительно высоты экрана.
+       * При снижении значения ниже MIN_PLAYER_TO_VIEWPORT_SCALE_FACTOR камера слегка сожмёт окно просмотра.
+       * При превышении выше MAX_PLAYER_TO_VIEWPORT_SCALE_FACTOR камера расширит окно просмотра. Если размеры 
+       * игрока меняются так, что масштабный коэффициент остаётся внутри интервала, то ничего не происходит*/
+      const float MIN_PLAYER_TO_VIEWPORT_SCALE_FACTOR = 0.3f;
+      const float MAX_PLAYER_TO_VIEWPORT_SCALE_FACTOR = 0.8f;
+      const float SCALE_FACTOR_AFTER_ADJUST = 0.5f;
+
+      if (TrackedPlayer == null || TrackedPlayer.Cells.Count == 0)
+        return;
+
+      float playerRadiusOnScreen = CalculateLineLengthInScreen(TrackedPlayer.Cells[0].Radius);
+      float scaleFactor = playerRadiusOnScreen / CameraHeight;
+      if (TrackedPlayer.Cells.Count == 1
+        && (scaleFactor < MIN_PLAYER_TO_VIEWPORT_SCALE_FACTOR
+        || scaleFactor > MAX_PLAYER_TO_VIEWPORT_SCALE_FACTOR))
+      {
+        CameraHeight = playerRadiusOnScreen / SCALE_FACTOR_AFTER_ADJUST;
+      }
+    }
+
+    /// <summary>
     /// Обновление камеры
     /// </summary>
     public override void Update()
     {
+      AdjustToPlayerSize();
       CenterOnTrackedPlayer();
     }
   }
